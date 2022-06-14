e7abdab8aecf6d43af7673755e91340f393d7a0e
Adam Januszewski <Adpjan@att.net>
2022-06-14 14:42:44 -0400

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by adam janusewski on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
